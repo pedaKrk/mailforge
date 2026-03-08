a320324289b0e44e646d122fa307fd1a85bdb720
Peter Karkulik <peter.karkulik@gmail.com>
2026-03-08 15:02:49 +0100

    add file upload endpoint

--- src/main/java/mailforge/api/EmailController.java
+++ src/main/java/mailforge/api/EmailController.java
@@ -1,16 +1,17 @@
 package mailforge.api;
 
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.*;
+import io.micronaut.http.multipart.CompletedFileUpload;
 
 @Controller("/email")
 public class EmailController {
 
-    @Get
-    @Produces(MediaType.TEXT_HTML)
-    public String index(){
-        return "Hello World";
+    @Post(value = "/parse", consumes = MediaType.MULTIPART_FORM_DATA)
+    public HttpResponse<String> parseEmail(CompletedFileUpload file) {
+        String filename = file.getFilename();
+
+        return HttpResponse.ok("Received file: " + filename);
     }
 }
