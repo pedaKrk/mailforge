7f2df3557cd79847f88e714b3beed23cd37f3d7d
Peter Karkulik <peter.karkulik@gmail.com>
2026-03-28 21:01:27 +0100

    add micronaut annotations

--- src/main/java/mailforge/service/parse/dto/BodyParserResult.java
+++ src/main/java/mailforge/service/parse/dto/BodyParserResult.java
@@ -1,7 +1,12 @@
 package mailforge.service.parse.dto;
 
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.serde.annotation.Serdeable;
+
 import java.util.List;
 
+@Introspected
+@Serdeable
 public record BodyParserResult(
     String textBody,
     String htmlBody,
--- src/main/java/mailforge/service/storage/dto/StoredAttachmentDto.java
+++ src/main/java/mailforge/service/storage/dto/StoredAttachmentDto.java
@@ -1,5 +1,10 @@
 package mailforge.service.storage.dto;
 
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.serde.annotation.Serdeable;
+
+@Introspected
+@Serdeable
 public record StoredAttachmentDto(
     String attachmentId,
     String originalFilename,
